9465ef6669d4412c333b69ea05cd54197de9c5d8
Fubohat <71642776+Fubohat@users.noreply.github.com>
2020-09-28 20:28:41 +0300

    4.5

---
+++ lab№4.5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	double  a, b;
+	cout << "a=";
+	cin >> a;
+	cout << "b=";
+	cin >> b;
+	cout << "sum=" << abs (a) + abs (b) << endl;
+    cout << "difference=" << abs(a) - abs(b) << endl;
+	cout << "composition=" << abs(a) * abs(b) << endl;
+	cout << "quotient=" << abs(a) / abs(b);
+}
